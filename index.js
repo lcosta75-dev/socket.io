572d1a57958366677c572117b2046573ef537613
Lais Costa <laiscosta@MacBook-Air-de-Lais.local>
2023-03-21 20:28:24 -0300

    Eventos

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const http = require("http").createServer(app)
 var io = require("socket.io")(http)
 
 io.on("connection", (socket) => {
-    console.log(socket)
-    console.log(socket.id)
+    socket.on("boasvindas", (data) => {
+        console.log("EXECUTANDO EVENTO DE BOAS VINDAS")
+        console.log(data)
+    })
+
+    socket.on("palavra", (data) => {
+        console.log(data)
+        socket.emit("resultado", data + " - GUIA DO PROGRAMADOR!")
+    })
 })
 
 app.set("view engine", "ejs")
